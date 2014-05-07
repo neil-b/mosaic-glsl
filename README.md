81248c4d8fee3ec5359f3ed73a0fec8d674161f6
neil <neil@neil-debian>
2014-05-07 15:39:29 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+About
+----
+Webpage and server that creates image mosaics from google search queries
+
+Installation
+----
+Install [node.js](nodejs.org) and run:
+
+```cd server
+npm install```
+
+Viewing the mosaic
+----
+Start the server with
+
+```cd server
+npm start```
+
+and go to [localhost:8080](http://localhost:8080)
+
+Libraries used
+----
+[three.js](https://github.com/mrdoob/three.js/)
+
+[jquery](https://github.com/jquery/jquery)
---
+++ server/server.js
@@ -0,0 +1,62 @@
+/*
+  Since cross origin textures aren't allowed in html5 canvases without the
+  texture provider's permission, this server, along with serving the contents
+  in the public directory, downloads and hosts image urls it receives from
+  the mosaic webpage.
+*/
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var fs = require('fs');
+
+var request = require('request');
+var mkdirp = require('mkdirp');
+
+io.set('log level', 1);
+server.listen(8080, '0.0.0.0');
+app.use(express.static(__dirname + '/../public'));
+app.use(express.static(__dirname + 'reup'));
+
+function download(url, outPath, callback) {
+  request.head(url, function(err, res, body) {
+    if (!err && res.statusCode == 200) {
+      var req = request(url).pipe(fs.createWriteStream(outPath));
+      req.on('close', callback);
+    }
+  });
+}
+
+io.sockets.on('connection', function(socket) {
+  socket.on('reuploadRequest', function(queries, urlStacks) {
+    var remainingReuploads = 0;
+    for (var i = 0; i < queries.length; i++) {
+      remainingReuploads += urlStacks[i].length;
+    }
+    // TODO: don't download if file already exists
+    var reuploadFolder = '../public/reup/';
+    for (var i = 0; i < queries.length; i++) {
+      for (var j = 0; j < urlStacks[i].length; j++) {
+        (function(i, j) {
+          mkdirp(reuploadFolder + queries[i] + '/', function(err) {
+            if (err) {
+              console.error(err);
+            }
+            else {
+              download(urlStacks[i][j], reuploadFolder + queries[i] + '/' + j, function() {
+                remainingReuploads--;
+                console.log('closed');
+                if (remainingReuploads == 0) {
+                  console.log('all done');
+                  socket.emit('reuploadComplete');
+                }
+              });
+            }
+          });
+        })(i, j);
+      }
+    }
+  });
+});
+
